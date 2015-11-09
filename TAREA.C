81e1deba4131b6f32a8e7f4786aab5c80d15bb5c
cecygomez95 <cecygomezroldan18@gmail.com>
2015-11-09 00:35:47 -0600

    FUP1C

---
+++ TAREA.C
@@ -0,0 +1,21 @@
+#include <studio.h>
+#include <stdlib.h>
+
+
+
+int main ()
+{
+	int numero=1;
+   
+    printf ("progrma que imprime los numero del 1 al 10 usando while\n\n");
+
+	while (numero <=10);
+	{
+	printf ("%d\n",numero);
+	num++;	
+    }
+    
+    printf ("\n\presione una tecla para salir..");
+    
+    system ("pause");
+}
