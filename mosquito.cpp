985e64c61ba6c898ce00c637dece1a33c951f5b1
cecygomez95 <cecygomezroldan18@gmail.com>
2015-12-02 02:04:54 -0600

    EVIDENCIA

---
+++ mosquito.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i;
+    char palabra[100];
+
+
+int main()
+{
+    
+    printf("Programa que cambia la palabra\n");
+    printf("Introduce el texto\n");
+    scanf("%s",palabra);
+    for(i=0;i<100;i++){
+                      if(palabra[i]=='a'){
+                                    palabra[i]='a';
+                                    }
+                      else if(palabra[i]=='e'){
+                                    palabra[i]='e';
+                                    }
+                      else if(palabra[i]=='i'){
+                                    palabra[i]='i';
+                                    }
+                      else if(palabra[i]=='o'){
+                                    palabra[i]='a';
+                                    }
+                      else if(palabra[i]=='u'){
+                                    palabra[i]='u';
+                                    }
+                      }
+    printf("%s\n",palabra);
+    system("PAUSE");
+}
